665622878cf98e7ee6374de7c5d22f186b468e8d
NormaArdhitaVidyiawati <147597223+NormaArdhitaVidyiawati@users.noreply.github.com>
2023-11-01 17:03:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+#show tittle
+print ("WELCOME TO OURS STORE")
+print ("___PIZZA CRUST___")
+
+print("Input Your Name: ")
+name = input() #get user name
+print("Input Your Table: ")
+table = input() #get user table
+
+total_hrga = 35000
+
+#showing topping menu
+print("Our Topping Menu, Please choose one")
+print("frankfuter bbq, meat monsta, super supreme, chiken lovers, cheese lovers, and american fav")
+topping = input("Enter our fav toppings: ")
+if topping == "frankfutter bbq" or topping == "meet monsta" or topping == "super supreme" or topping == "chiken lovers" or topping == "cheese lovers" or topping == "american fav":
+    total_hrga = 50000 #get topping price
+else:
+    total_hrga = 0
+  print("Sorry, your fav topping was sold out/nothing")
+
 #showing and inputing crust menu 
 print("Our Crust Menu, Pelase choose one ")
 print("""original,
